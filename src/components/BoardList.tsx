57eb55dcd004aa52ea95533e8fc7d2e0c6d38540
codyarose <crose992@gmail.com>
2020-02-27 13:38:26 -0600

    Refactored BoardList to use Link

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -1,40 +1,40 @@
 import React, { useEffect } from 'react'
 import { connect, useDispatch } from 'react-redux'
-import { v4 as uuidv4 } from 'uuid'
+// import { v4 as uuidv4 } from 'uuid'
 import { fetchBoards } from 'redux/actions/boardActions'
+import { Link } from 'react-router-dom'
 
 interface Props {
 
 }
 
-const BoardList: React.FC<Props> = ({ error, isFetching, boards }: any) => {
+const BoardList: React.FC<Props> = ({ error, isLoading, boards }: any) => {
 	const dispatch = useDispatch()
 
 	useEffect(() => {
 		fetchBoards()
 	}, [dispatch])
 
-	if (error) {
-		return <div>Error: {error.message}</div>
-	}
-	if (isFetching) {
-		return <div>Fetching...</div>
-	}
-
 	return (
 		<>
-			<ul>
-				{boards.map((board: any) =>
-					<li key={board.id}>{board.name}</li>
-				)}
-			</ul>
+			{error && <div>Error: {error.message}</div>}
+			{isLoading && <div>Loading...</div>}
+			{!error && !isLoading && boards &&
+				<ul>
+					{boards.map((board: any) =>
+						<li key={board.shortLink}>
+							<Link to={`/board/${board.shortLink}`}>{board.name}</Link>
+						</li>
+					)}
+				</ul>
+			}
 		</>
 	)
 }
 
 const mapStateToProps = (state: any) => ({
 	boards: state.boards.items,
-	isFetching: state.boards.isFetching,
+	isLoading: state.boards.isLoading,
 	error: state.boards.error
 })
 
