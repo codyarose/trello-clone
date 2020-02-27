b90b7fb7991a3d8b5a5494853650b4b221b33948
codyarose <crose992@gmail.com>
2020-02-27 14:29:24 -0600

    Added fetchBoard to Board

--- src/routes/Board.tsx
+++ src/routes/Board.tsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect, useDispatch } from 'react-redux'
+import { fetchBoard } from 'redux/actions/boardActions'
 import { useParams } from 'react-router-dom'
 
 interface Props {
 
 }
 
-const Board: React.FC<Props> = () => {
+const Board: React.FC<Props> = ({ error, isLoading, board }: any) => {
+	const dispatch = useDispatch()
+
 	const { id } = useParams()
 
+	useEffect(() => {
+		id && dispatch(fetchBoard(id))
+	}, [id])
+
 	return (
-		<div>
-			Board: {id}
-		</div>
+		<>
+			{error && <div>Error: {error.message}</div>}
+			{isLoading && <div>Loading...</div>}
+			{!error && !isLoading && board &&
+				<div>
+					Board: {board && board.name}
+				</div>
+			}
+		</>
 	)
 }
 
-export default Board
+const mapStateToProps = (state: any) => ({
+	board: state.board.data,
+	isLoading: state.board.isLoading,
+	error: state.board.error
+})
+
+const mapDispatchToProps = {
+	fetchBoard
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Board)
