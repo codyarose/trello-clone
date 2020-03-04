0b3d0b922bbd3925136c44284cb203be7498f2a0
codyarose <crose992@gmail.com>
2020-03-04 13:42:10 -0600

    Added mock labels and temporary track

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -1,17 +1,33 @@
 import React, { FC } from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 // import { v4 as uuidv4 } from 'uuid'
 
 import { RootState } from 'redux/modules/allBoards'
-import { Card } from 'components/common/Card'
+import { Card } from './common/Card'
 
 export const BoardList: FC<RootState> = ({ error, isLoading, items }) => {
+	const labels = [
+		{
+			"id": "5e4d7961af988c41f28160c8",
+			"idBoard": "5e4d79614e7f3f847c08b832",
+			"name": "Component",
+			"color": "blue"
+		},
+		{
+			"id": "5e4d7961af988c41f2lsjdii",
+			"idBoard": "5e4d79614e7f3f847c08b832",
+			"name": "Util",
+			"color": "green"
+		}
+	]
+
 	return (
 		<>
 			{error && <div>Error: {error.message}</div>}
 			{isLoading && <div>Loading...</div>}
 			{items &&
-				<ul>
+				<StyledTrack>
 					{items.map((board: any) =>
 						<li key={board.id}>
 							<Link to={`/board/${board.id}`}>
@@ -22,12 +38,22 @@ export const BoardList: FC<RootState> = ({ error, isLoading, items }) => {
 									members={board.memberships}
 									background={board.prefs.background}
 									imgUrl="https://images.unsplash.com/photo-1583336490127-4f08ed1e93dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=10"
+									labels={labels}
 								/>
 							</Link>
 						</li>
 					)}
-				</ul>
+				</StyledTrack>
 			}
 		</>
 	)
 }
+
+const StyledTrack = styled.ul`
+	list-style-type: none;
+	margin: 0;
+	padding: 20px;
+	display: grid;
+	grid-template-columns: repeat(auto-fill, 362px);
+	gap: 30px;
+`
