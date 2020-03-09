978d78f5b6ea7a7d5d5c4b052fc4359e83afb7c4
codyarose <crose992@gmail.com>
2020-03-09 16:40:17 -0500

    Added Track content styles

--- src/components/common/Track.tsx
+++ src/components/common/Track.tsx
@@ -10,10 +10,10 @@ interface Props {
 export const Track: FC<Props> = ({ children }) => {
 	return (
 		<TrackContainer>
-			<TrackHeader>Header</TrackHeader>
-			<TrackContents>
+			<TrackHeader variant="xl">Recently viewed</TrackHeader>
+			<TrackContent>
 				{children}
-			</TrackContents>
+			</TrackContent>
 		</TrackContainer>
 	)
 }
@@ -23,9 +23,28 @@ const TrackContainer = styled.div`
 `
 
 const TrackHeader = styled(Typography)`
-	/*  */
+	margin-bottom: 1rem;
 `
 
-const TrackContents = styled.div`
-	/*  */
+const TrackContent = styled.ul`
+	list-style-type: none;
+	padding: 0 0 1.5rem;
+	margin: 0;
+	display: grid;
+	grid-auto-columns: 362px;
+	grid-auto-flow: column;
+	gap: 30px;
+	overflow-x: auto;
+
+	::-webkit-scrollbar {
+		height: 12px;
+	}
+	::-webkit-scrollbar-track {
+		background-color: rgba(${({ theme }) => theme.accent}, .06);
+		border-radius: 10px;
+	}
+	::-webkit-scrollbar-thumb {
+		background-color: rgba(${({ theme }) => theme.accent}, .3);
+		border-radius: 10px;
+	}
 `
