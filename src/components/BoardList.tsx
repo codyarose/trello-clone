0a9432dcf3950a5b81ec7dddb199e96c7a313380
codyarose <crose992@gmail.com>
2020-03-10 14:45:35 -0500

    Refactored BoardList to be more simple, less like a container component

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -2,15 +2,16 @@ import React, { FC } from 'react'
 import { Link } from 'react-router-dom'
 // import { v4 as uuidv4 } from 'uuid'
 
-import { RootState } from 'redux/modules/allBoards'
 import { Card } from './common/Card'
 import { NewCard } from './common/NewCard'
 
-export const BoardList: FC<RootState> = ({ error, isLoading, items }) => {
+interface Props {
+	items: any[]
+}
+
+export const BoardList: FC<Props> = ({ items }) => {
 	return (
 		<>
-			{error && <div>Error: {error.message}</div>}
-			{isLoading && <div>Loading...</div>}
 			{items && items.map((board: any) =>
 				<li key={board.id}>
 					<Link to={`/board/${board.id}`}>
